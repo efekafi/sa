c2bc81e624506b19b0cf3138cee437086e7d9f9b
efekafi <97988678+efekafi@users.noreply.github.com>
2025-11-21 16:45:42 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // GitHub Pages gibi alt klasörlerde çalışması için base yolunu relative yapıyoruz
-  base: './', 
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
